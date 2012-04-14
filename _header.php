be0491c28449a3b9e375544e59dbab0b085199f6
Benjamin Fisher <neohell@hotmail.com>
2012-04-13 17:12:27 -0700

    PHP conversion in progress

--- _header.php
+++ _header.php
@@ -1,12 +1,8 @@
+<?php 
+	$current_page = strstr(basename($_SERVER['PHP_SELF']), '.', TRUE);
+?>
+
 <!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>Solus - Home</title>
-	
-	<link rel="stylesheet" href="stylesheets/screen.css" media="screen, projection" rel="stylesheet" />
-	
-	<!DOCTYPE HTML>
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
@@ -16,45 +12,23 @@
 	
 	<script src="http://cdnjs.cloudflare.com/ajax/libs/headjs/0.96/head.min.js"></script>
 </head>
-<body class="home">
+<body class="<?php echo $current_page ?>">
 	<header>
-		<a href="index.html"><h1>Solus</h1></a>
+		<a href="index.php"><h1>Solus</h1></a>
 		<nav>
 			<ul>
-				<li class="current"><a href="index.html">Home</a></li>
-				<li><a href="about.html">About</a>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="about.php">About</a>
 					<ul class="sub">
-						<li><a href="people.html">People</a></li>
+						<li><a href="people.php">People</a></li>
 						<li><a href="#">Green Initiative</a></li>
 						<li><a href="#">Territory</a></li>
 					</ul>
 				</li>
-				<li><a href="linecard.html">Line Card</a></li>
-				<li><a href="samplelibrary.html">Sample Library</a></li>
+				<li><a href="line-card.php">Line Card</a></li>
+				<li><a href="sample-library.php">Sample Library</a></li>
 			</ul>
 		</nav>
 	</header>
-	
-	<div class="content">
-</head>
-<body class="home">
-	<header>
-		<a href="index.html"><h1>Solus</h1></a>
-		<nav>
-			<ul>
-				<li class="current"><a href="index.html">Home</a></li>
-				<li><a href="about.html">About</a>
-					<ul class="sub">
-						<li><a href="people.html">People</a></li>
-						<li><a href="#">Green Initiative</a></li>
-						<li><a href="#">Territory</a></li>
-					</ul>
-				</li>
-				<li><a href="linecard.html">Line Card</a></li>
-				<li><a href="samplelibrary.html">Sample Library</a></li>
-			</ul>
-		</nav>
-	</header>
-	
 	<!-- Begin Content Div (ends in _footer.php) -->
 	<div class="content">
