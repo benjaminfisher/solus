8edb691e3c6b2835e737a31a317716634be829f4
Benjamin Fisher <neohell@hotmail.com>
2012-04-23 14:36:44 -0700

    Columnz working in IE, without hide.

--- scripts/columnFix.js
+++ scripts/columnFix.js
@@ -2,8 +2,8 @@
 	var $columns = $('div.columns');
 
 	if($columns.hasClass('3')){
-		$columns.columnize( {columns:3} ).show();
+		$columns.columnize( {columns:3} );
 	} else {
-		$columns.columnize( {columns:2} ).show();
+		$columns.columnize( {columns:2} );
 	};
 })();
