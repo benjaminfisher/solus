d4220554a0f739f7ae3ed5e072131992c3400648
Benjamin Fisher <neohell@hotmail.com>
2012-04-25 14:12:34 -0700

    Styling for samples page

--- ametrix_samples.php
+++ ametrix_samples.php
@@ -10,12 +10,12 @@ include "_sample_form.php";
 	
 	<div class="copy">
 		<h2><span class="upper"><?php echo $product; ?></span> Samples:</h2>
-		<a href="sample-library.php">(return to samples)</a>
+		<a href="sample-library.php" class="return">(return to samples)</a>
 		
 		<ul class="samples">
 			<li>
 				<a href="http://www.ametrixlighting.com/common/brands.cfm?pg=Detail&brandName=Ametrix&category=Base:%20Arrowlinear&id=17130">
-					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="" />
+					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="image of sample <?php echo $productCount; ?>" />
 					<h3 class="upper"><?php echo $product.'-'.$productCount; ?></h3>
 				</a>
 				<p class="upper">ARROWLINEAR SAMPLE KIT - A03-SO-A-1-LF-039-120-S-IL35</p>
@@ -23,7 +23,7 @@ include "_sample_form.php";
 			</li>
 			<li>
 				<a href="http://www.ametrixlighting.com/">
-					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="" />
+					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="image of sample <?php echo $productCount; ?>" />
 					<h3 class="upper"><?php echo $product.'-'.$productCount; ?></h3>
 				</a>
 				<p class="upper">AMETRIX SALES KIT:  SN-SO-P-1-T-100-120-S-YR; VS-SO-P-1-T-100-120-S-YR</p>
@@ -31,7 +31,7 @@ include "_sample_form.php";
 			</li>
 			<li>
 				<a href="http://www.ametrixlighting.com/common/brands.cfm?pg=Products&brandName=Ametrix&group=&category=Ceiling%20-%20Surface:%20C-Scroll">
-					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="" />
+					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="image of sample <?php echo $productCount; ?>" />
 					<h3 class="upper"><?php echo $product.'-'.$productCount; ?></h3>
 				</a>
 				<p class="upper">C-SCROLL LINEAR:  SN-SI-P-1-LF-054-120-W-YI-L45</p>
@@ -39,7 +39,7 @@ include "_sample_form.php";
 			</li>
 			<li>
 				<a href="http://www.ametrixlighting.com/common/brands.cfm?pg=Products&brandName=Ametrix&group=&category=Ceiling%20-%20Surface:%20C-Scroll">
-					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="" />
+					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="image of sample <?php echo $productCount; ?>" />
 					<h3 class="upper"><?php echo $product.'-'.$productCount; ?></h3>
 				</a>
 				<p class="upper">C-SCROLL LINEAR:  SN-SI-P-1-LF-054-120-W-YI-L45</p>
@@ -47,7 +47,7 @@ include "_sample_form.php";
 			</li>
 			<li>
 				<a href="http://www.ametrixlighting.com/common/brands.cfm?pg=Products&brandName=Ametrix&group=&category=Ceiling%20-%20Surface:%20Vault">
-					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="" />
+					<img src=<?php print '"assets/products/'.$product.'_'.$productCount.'.jpg"'; ?> alt="image of sample <?php echo $productCount; ?>" />
 					<h3 class="upper"><?php echo $product.'-'.$productCount; ?></h3>
 				</a>
 				<p class="upper">VAULT LINEAR:  VN-SI-P-1-LF-054-120-W-YI-L45</p>
